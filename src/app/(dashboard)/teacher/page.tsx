d3f090b6f6668b526ca9bf496d8ade4bbec7404c
Rohit Kumar <rk.0010.dev@gmail.com>
2025-03-17 11:04:53 +0530

    Frontend1

--- src/app/(dashboard)/teacher/page.tsx
+++ src/app/(dashboard)/teacher/page.tsx
@@ -1,13 +1,12 @@
 import Announcements from "@/components/Announcements"
-import BigCalendar from "@/components/BigCalendar"
-import EventCalendar from "@/components/EventCalendar"
+import BigCalendar from "@/components/BigCalender"
 
 const TeacherPage = () => {
     return (
-        <div className='p-4 flex gap-4 flex-col xl:flex-row'>
+        <div className='flex-1 p-4 flex gap-4 flex-col xl:flex-row'>
             {/* LEFT */}
             <div className="w-full xl:w-2/3">
-                <div className="h-full bg-white p-4 rounded-md">
+                <div className="h-screen bg-white p-4 rounded-md">
                     <h1 className="text-xl font-semibold">Schedule</h1>
                     <BigCalendar />
                 </div>
--- src/components/BigCalender.tsx
+++ src/components/BigCalender.tsx
@@ -1,12 +1,12 @@
-"use client"
+"use client";
 
-import { Calendar, momentLocalizer, View, Views } from 'react-big-calendar';
-import moment from 'moment';
+import { Calendar, momentLocalizer, View, Views } from "react-big-calendar";
+import moment from "moment";
 import { calendarEvents } from "@/lib/data";
-import 'react-big-calendar/lib/css/react-big-calendar.css';
-import { useState } from 'react';
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { useState } from "react";
 
-const localizer = momentLocalizer(moment)
+const localizer = momentLocalizer(moment);
 
 const BigCalendar = () => {
     const [view, setView] = useState<View>(Views.WORK_WEEK);
@@ -14,6 +14,7 @@ const BigCalendar = () => {
     const handleOnChangeView = (selectedView: View) => {
         setView(selectedView);
     };
+
     return (
         <Calendar
             localizer={localizer}
